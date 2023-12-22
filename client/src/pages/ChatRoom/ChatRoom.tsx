eac32775afaf174566170c66d42e9188a3df4870
Tyrone Saulon <your_email@example.com>
2023-12-21 21:28:19 -0500

    feature: add handlers for sending or creating messages

--- client/src/pages/ChatRoom/ChatRoom.tsx
+++ client/src/pages/ChatRoom/ChatRoom.tsx
@@ -2,19 +2,43 @@ import { Card, Container, Stack } from '@mantine/core';
 import { ChatRoomHeader } from './components/ChatRoomHeader/ChatRoomHeader.tsx';
 import { ChatRoomMessages } from './components/ChatRoomMessages/ChatRoomMessages.tsx';
 import { ChatRoomInput } from './components/ChatRoomInput/ChatRoomInput.tsx';
+import {
+  useCreateMessageMutation,
+  useLoadChatRoomQuery,
+} from '../../graphql.types.tsx';
+import { getMessagesWithUser, getTotalParticipants } from './ChatRoom.utils.ts';
+import { useUserContext } from '../../contexts/UserContext/useUserContext.ts';
 
 import * as SharedClasses from '../../shared/shared.module.css';
 import * as Classes from './ChatRoom.module.css';
 
 export function ChatRoom() {
+  const { currentUser } = useUserContext();
+  const { data, loading } = useLoadChatRoomQuery();
+  const [createMessage, { loading: sending }] = useCreateMessageMutation();
+  const messages = getMessagesWithUser(data);
+  const totalParticipants = getTotalParticipants(data);
+
+  const handleSend = async (text: string) => {
+    await createMessage({
+      variables: {
+        text,
+        user_id: currentUser?.id,
+      },
+    });
+  };
+
   return (
     <Container classNames={{ root: SharedClasses.page }} size="xs">
       <Stack className={Classes.messageContainer}>
         <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <ChatRoomHeader users={[]} />
-          <ChatRoomMessages messages={[]} />
+          <ChatRoomHeader
+            totalParticipants={totalParticipants}
+            loading={loading}
+          />
+          <ChatRoomMessages messages={messages} />
         </Card>
-        <ChatRoomInput onSend={() => {}} />
+        <ChatRoomInput onSend={handleSend} sending={sending} />
       </Stack>
     </Container>
   );
