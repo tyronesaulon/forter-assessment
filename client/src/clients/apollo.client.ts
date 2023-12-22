1651e715491f9802040c2a4839085eaba8b97d9a
Tyrone Saulon <your_email@example.com>
2023-12-21 20:27:36 -0500

    refactor: attach admin secret key to apollo client

--- client/src/clients/apollo.client.ts
+++ client/src/clients/apollo.client.ts
@@ -1,8 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-const { VITE_GRAPHQL_URL } = import.meta.env;
+const { VITE_GRAPHQL_URL, VITE_GRAPHQL_ADMIN_SECRET_KEY: adminSecretKey } =
+  import.meta.env;
+
+const httpLink = createHttpLink({
+  uri: VITE_GRAPHQL_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'x-hasura-admin-secret': adminSecretKey,
+    },
+  };
+});
 
 export const apollo = new ApolloClient({
-  uri: VITE_GRAPHQL_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
